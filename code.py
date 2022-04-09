8422cbb94770964fa186802fcc277525d2e362c3
rik-code <topelven1234567890@gmail.com>
2022-04-09 20:37:08 +0300

    Adding sticker and greeting👋

--- code.py
+++ code.py
@@ -4,6 +4,13 @@ import config
 bot = telebot.TeleBot(config.TOKEN)
 
 
+@bot.message_handler(commands=['start'])
+def welcome(message):
+    stick = open('sticker_telegram/sticker.webp', 'rb')
+    bot.send_sticker(message.chat.id, stick)
+    bot.send_message(message.chat.id, 'Приветик..., {0.first_name}!\n - <b{1.first_name}<\b>, бот созданный для того чтобы говорить тебе привет и другое.'.format(message.from_user, bot.get_me()), parse_mode='html')
+
+
 @bot.message_handler(content_types=['text'])
 def bot1(message):
     bot.send_message(message.chat.id, message.text)
