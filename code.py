c958cae5341fcfea07b0c385954bc351fbacdc2f
rik-code <topelven1234567890@gmail.com>
2022-04-09 20:43:41 +0300

    Fixing program🛠

--- code.py
+++ code.py
@@ -8,7 +8,7 @@ bot = telebot.TeleBot(config.TOKEN)
 def welcome(message):
     stick = open('sticker_telegram/sticker.webp', 'rb')
     bot.send_sticker(message.chat.id, stick)
-    bot.send_message(message.chat.id, 'Приветик..., {0.first_name}!\n - <b{1.first_name}<\b>, бот созданный для того чтобы говорить тебе привет и другое.'.format(message.from_user, bot.get_me()), parse_mode='html')
+    bot.send_message(message.chat.id, 'Приветик, {0.first_name}!\n - <b>{1.first_name}</b>, бот созданный для того чтобы говорить тебе привет и другое.'.format(message.from_user, bot.get_me()), parse_mode='html')
 
 
 @bot.message_handler(content_types=['text'])
